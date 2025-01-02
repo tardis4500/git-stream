7e44a529e800002c03f853c8f7c91179b90e3117
Jeff <web@pobox.com>
2025-01-01 20:40:37 -0500

    Upgrade to Python 3.12.

--- util/update-env.sh
+++ util/update-env.sh
@@ -3,5 +3,6 @@ set -eu
 
 python -m pip install --upgrade pip
 pip install --upgrade --upgrade-strategy eager setuptools wheel
+pip install --upgrade --upgrade-strategy eager flit
 pip freeze | grep -v '^\-e' | cut -d = -f 1 | xargs -n1 pip install --upgrade
-flit install -s --deps all
+flit install -s --only-deps --deps all
