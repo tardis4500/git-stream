915181f1ea3abcbb061ca73fc2319a5ce19ec3eb
Jeff <web@pobox.com>
2026-07-03 11:16:40 -0400

    Update rc tag. [skip cl]

--- git_stream/__init__.py
+++ git_stream/__init__.py
@@ -13,7 +13,7 @@ __title__ = 'git-stream'
 __summary__ = 'Git Stream Implementation'
 __uri__ = 'https://github.com/arisilon/git-stream/'
 
-__version__ = '4.0.0rc0'
+__version__ = '4.0.0rc1'
 __build_name__ = '{var:build_name}'
 __build_date__ = '{var:build_date}'
 
